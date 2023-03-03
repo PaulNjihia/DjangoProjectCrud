e36c4e5534bacc7eabd9dd4143b010ad1e720c33
PaulNjihia <njorogenjihiap@gmail.com>
2023-03-03 10:50:25 +0300

    second commit

--- morningDjangoProject/views.py
+++ morningDjangoProject/views.py
@@ -11,6 +11,9 @@ def register(request):
             form.save()
             messages.success(request, 'Account created successfully!')
             return redirect("register")
+        else:
+            messages.error(request, 'Account creation failed')
+            return redirect("register")
     else:
         form = UserRegistrationForm()
     return render(request, 'register.html', {'form':form})
@@ -19,6 +22,7 @@ def register(request):
 def home(request):
     return render(request, 'home.html')
 
+@login_required
 def add_product(request):
     if request.method =="POST":
         p_name = request.POST.get("jina")
@@ -29,3 +33,47 @@ def add_product(request):
         messages.success(request, 'Product saved successfully')
         return redirect('add-product')
     return render(request, 'add-product.html')
+
+@login_required
+def view_products(request):
+
+    #select all the products from the database
+    products = Product.objects.all()
+
+    #render the template with the prodicts
+    return render(request, 'products.html', {'products': products})
+
+@login_required
+def delete_product(request, id):
+    #select the product to delete
+    product = Product.objects.get(id=id)
+    #finally delete the product
+    product.delete()
+    #redirect back to product page with a success message
+    messages.success(request, 'Product deleted successfully')
+    return redirect('products')
+
+@login_required
+def update_product(request,id):
+    #select the product to be updated
+    product = Product.objects.get(id=id)
+
+    #check if the form has any submitted records to recieve them
+    if request.method == "POST":
+        updated_name = request.POST.get('jina')
+        updated_quantity = request.POST.get('kiasi')
+        updated_price = request.POST.get('bei')
+
+        #update the selected product above with the recieved data
+        product.prod_name = updated_name
+        product.prod_quantity = updated_quantity
+        product.prod_price = updated_price
+
+        #return the updated data back to the database
+        product.save()
+
+        #redirect back to the products page with a success message
+        messages.success(request, 'Product updated successfully')
+        return redirect('products')
+
+    return render(request, 'update-product.html', {'product': product})
